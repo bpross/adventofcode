c4f3b5b40a0fb3b64e9436c67a769a27804ef742
Benjamin Ross <ben.ross@moov.io>
2024-12-02 10:16:32 -0700

    got part two working and added array helper

--- 2024/2/solution.go
+++ 2024/2/solution.go
@@ -8,67 +8,101 @@ import (
 	"github.com/bpross/adventofcode/utils"
 )
 
+func checkIfSafe(nums []string) bool {
+	increase, decrease := false, false
+	i, j := 0, 1
+	iInt, err := strconv.Atoi(nums[i])
+	if err != nil {
+		panic(err)
+	}
+
+	jInt, err := strconv.Atoi(nums[j])
+	if err != nil {
+		panic(err)
+	}
+	if iInt > jInt {
+		decrease = true
+	} else {
+		increase = true
+	}
+
+	for j < len(nums) {
+		iInt, err = strconv.Atoi(nums[i])
+		if err != nil {
+			panic(err)
+		}
+
+		jInt, err = strconv.Atoi(nums[j])
+		if err != nil {
+			panic(err)
+		}
+
+		if iInt == jInt {
+			return false
+		}
+		if jInt < iInt && increase {
+			return false
+		}
+		if jInt > iInt && decrease {
+			return false
+		}
+
+		diff := utils.Abs(iInt, jInt)
+		if diff < 1 || diff >= 4 {
+			return false
+		}
+
+		i++
+		j++
+	}
+
+	return true
+}
+
 func part1() {
 	total := 0
 
-	lineFunc := func(line string, lineNumber int) error {
-		fmt.Println("line", lineNumber)
-		increase, decrease := false, false
+	lineFunc := func(line string, _ int) error {
 		nums := strings.Split(line, " ")
 
-		i, j := 0, 1
-		iInt, err := strconv.Atoi(nums[i])
-		if err != nil {
-			panic(err)
+		safe := checkIfSafe(nums)
+		if safe {
+			total += 1
 		}
 
-		jInt, err := strconv.Atoi(nums[j])
-		if err != nil {
-			panic(err)
-		}
-		if iInt > jInt {
-			decrease = true
-		} else {
-			increase = true
-		}
+		return nil
+	}
 
-		for j < len(nums) {
-			iInt, err = strconv.Atoi(nums[i])
-			if err != nil {
-				panic(err)
-			}
+	err := utils.ReadFile("input.txt", lineFunc)
+	if err != nil {
+		panic(err)
+	}
 
-			jInt, err = strconv.Atoi(nums[j])
-			if err != nil {
-				panic(err)
-			}
+	fmt.Println(total)
+}
 
-			if iInt == jInt {
-				fmt.Println("Equal", nums[j], nums[i], line)
-				// equal, needs to increase OR decrease
-				return nil
-			}
-			if jInt < iInt && increase {
-				fmt.Println("not increasing", nums[j], nums[i], line)
-				return nil
-			}
-			if jInt > iInt && decrease {
-				fmt.Println("not decreasing", nums[j], nums[i], line)
-				return nil
-			}
+func part2() {
+	total := 0
 
-			diff := utils.Abs(iInt, jInt)
-			if diff < 1 || diff >= 4 {
-				fmt.Println("diff", iInt, jInt, line, diff)
-				return nil
-			}
+	lineFunc := func(line string, _ int) error {
+		nums := strings.Split(line, " ")
 
-			i++
-			j++
+		safe := checkIfSafe(nums)
+		if safe {
+			total += 1
+			return nil
 		}
 
-		// Safe
-		total += 1
+		for i := 0; i < len(nums); i++ {
+			modified := nums
+			modified = utils.RemoveIndex(modified, i)
+			safe = checkIfSafe(modified)
+			if safe {
+				total += 1
+				break
+			}
+		}
+
 		return nil
 	}
 
@@ -82,4 +116,5 @@ func part1() {
 
 func main() {
 	part1()
+	part2()
 }
---
+++ utils/slice.go
@@ -0,0 +1,7 @@
+package utils
+
+func RemoveIndex(s []string, index int) []string {
+	ret := make([]string, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
+}
