8f3e7409161aa38b32278be0e1794ef2453cea08
Benjamin Ross <ben.ross@moov.io>
2024-12-03 11:22:49 -0700

    got part 2

--- 2024/3/solution.go
+++ 2024/3/solution.go
@@ -36,6 +36,52 @@ func part1() {
 	fmt.Println(total)
 }
 
+func part2() {
+	total := 0
+	multiply := true
+	lineFunc := func(line string, _ int) error {
+		mul := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)`)
+		matches := mul.FindAllStringSubmatch(line, -1)
+
+		for _, match := range matches {
+			if match[0] == "do()" {
+				fmt.Println("enabled", match)
+				multiply = true
+				continue
+			} else if match[0] == "don't()" {
+				fmt.Println("disabled", match)
+				multiply = false
+				continue
+			}
+
+			if multiply {
+				fmt.Println("adding to total", match)
+				i, err := strconv.Atoi(match[1])
+				if err != nil {
+					panic(err)
+				}
+				j, err := strconv.Atoi(match[2])
+				if err != nil {
+					panic(err)
+				}
+
+				total += i * j
+			} else {
+				fmt.Println("skipped", match)
+			}
+		}
+
+		return nil
+	}
+
+	err := utils.ReadFile("input.txt", lineFunc)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(total)
+}
+
 func main() {
 	part1()
+	part2()
 }
