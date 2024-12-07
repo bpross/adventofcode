2ec027f3df6980d8d73df65472c9de49106f1064
Benjamin Ross <ben.ross@moov.io>
2024-12-07 14:09:12 -0700

    got part 2

--- 2024/7/solution.go
+++ 2024/7/solution.go
@@ -10,49 +10,80 @@ import (
 
 type equation struct {
 	total  int
-	values []int
+	values []string
 }
 
 // generate all permutations of values and operations
-func permutations(values []string, wantLength int) []string {
+func permutations(values []string, wantLength int) [][]string {
 	found := make(map[string]bool)
-	res := make([]string, 0)
+	res := make([][]string, 0)
 
-	var dfs func(path string)
-	dfs = func(path string) {
-		if len(path) == wantLength {
+	var dfs func(path string, ops int)
+	dfs = func(path string, ops int) {
+		if ops == wantLength {
 			if _, ok := found[path]; ok {
 				return
 			}
 			found[path] = true
-			fmt.Println("Path:", path)
-			res = append(res, path)
+			var allOps []string
+			idx := 0
+			for idx < len(path) {
+				if path[idx] == '|' {
+					allOps = append(allOps, "||")
+					idx += 2
+				} else {
+					allOps = append(allOps, string(path[idx]))
+					idx++
+				}
+			}
+			res = append(res, allOps)
 			return
 		}
 		for i := 0; i < len(values); i++ {
-			fmt.Println("Path:", path, "Values:", values[i])
-			dfs(path + values[i])
+			ops++
+			dfs(path+values[i], ops)
+			ops--
 		}
 	}
-	dfs("")
+	dfs("", 0)
 
 	return res
 }
 
-func checkPermutations(e equation, perms []string) bool {
+func checkPermutations(e equation, perms [][]string) bool {
 	for _, p := range perms {
-		total := e.total
-		for i, v := range e.values {
-			if i == 0 {
-				total = v
+		total := 0
+		idx := 0
+		for idx < len(e.values) {
+			k, err := strconv.Atoi(e.values[idx])
+			if err != nil {
+				panic(err)
+			}
+
+			if idx == 0 {
+				total = k
+				idx++
 				continue
 			}
-			if p[i-1] == '+' {
-				total += v
-			} else {
-				total *= v
+
+			switch p[idx-1] {
+			case "+":
+				idx++
+				total += k
+			case "*":
+				idx++
+				total *= k
+			case "||":
+				t := strconv.Itoa(total)
+				t += e.values[idx]
+				total, err = strconv.Atoi(t)
+				if err != nil {
+					panic(err)
+				}
+				idx++
 			}
 		}
+
 		if total == e.total {
 			return true
 		}
@@ -72,13 +103,9 @@ func part1() {
 
 		p := strings.Trim(nums[1], " ")
 		products := strings.Split(p, " ")
-		vals := make([]int, 0)
+		vals := make([]string, 0)
 		for _, v := range products {
-			val, err := strconv.Atoi(v)
-			if err != nil {
-				panic(err)
-			}
-			vals = append(vals, val)
+			vals = append(vals, v)
 		}
 
 		e := equation{total: total, values: vals}
@@ -96,9 +123,6 @@ func part1() {
 		perms := permutations(ops, len(e.values)-1)
 		if checkPermutations(e, perms) {
 			trueEquations = append(trueEquations, e)
-		} else {
-			fmt.Println(e)
-			fmt.Println(perms)
 		}
 	}
 
@@ -111,6 +135,50 @@ func part1() {
 	fmt.Println(total)
 }
 
+func part2() {
+	equations := make([]equation, 0)
+
+	lineFunc := func(line string, _ int) error {
+		nums := strings.Split(line, ":")
+		total, err := strconv.Atoi(nums[0])
+		if err != nil {
+			panic(err)
+		}
+
+		p := strings.Trim(nums[1], " ")
+		products := strings.Split(p, " ")
+		vals := make([]string, 0)
+		for _, v := range products {
+			vals = append(vals, v)
+		}
+
+		e := equation{total: total, values: vals}
+		equations = append(equations, e)
+		return nil
+	}
+	err := utils.ReadFile("input.txt", lineFunc)
+	if err != nil {
+		panic(err)
+	}
+
+	trueEquations := make([]equation, 0)
+	ops := []string{"+", "*", "||"}
+	for _, e := range equations {
+		perms := permutations(ops, len(e.values)-1)
+		if checkPermutations(e, perms) {
+			trueEquations = append(trueEquations, e)
+		}
+	}
+
+	total := int64(0)
+	for _, e := range trueEquations {
+		total += int64(e.total)
+	}
+
+	fmt.Println(total)
+}
+
 func main() {
-	part1()
+	// part1()
+	part2()
 }
