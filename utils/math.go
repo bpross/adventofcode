f08f6ecbe29e969dfa11a2943b3d9edb08f6d3d3
Benjamin Ross <ben.ross@moov.io>
2024-12-07 12:09:03 -0700

    more util code

--- utils/math.go
+++ utils/math.go
@@ -6,3 +6,18 @@ func Abs(x, y int) int {
 	}
 	return x - y
 }
+
+func Factorial(number int) int {
+	// if the number has reached 1 then we have to
+	// return 1 as 1 is the minimum value we have to multiply with
+	if number <= 1 {
+		return 1
+	}
+
+	// multiplying with the current number and calling the function
+	// for 1 lesser number
+	factorialOfNumber := number * Factorial(number-1)
+
+	// return the factorial of the current number
+	return factorialOfNumber
+}
