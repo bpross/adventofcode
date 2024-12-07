ef0e59629ac8774a9f395be2a7499fae6929fa0c
Benjamin Ross <ben.ross@moov.io>
2024-12-06 20:06:23 -0700

    got part 2

--- 2024/6/solution.go
+++ 2024/6/solution.go
@@ -10,7 +10,19 @@ type position struct {
 	r, c int
 }
 
-func positionValid(grid []string, pos position) bool {
+type cyclePosition struct {
+	position
+	direction rune
+}
+
+var positionToRight = map[rune]rune{
+	'^': '>',
+	'>': 'v',
+	'v': '<',
+	'<': '^',
+}
+
+func positionValid(grid [][]string, pos position) bool {
 	if pos.r < 0 || pos.r >= len(grid) {
 		return false
 	}
@@ -20,68 +32,120 @@ func positionValid(grid []string, pos position) bool {
 	return true
 }
 
-func positionObstacle(grid []string, pos position) bool {
+func positionObstacle(grid [][]string, pos position) bool {
 	if !positionValid(grid, pos) {
 		return false
 	}
 
-	return grid[pos.r][pos.c] == '#'
+	return grid[pos.r][pos.c] == "#"
 }
 
-func getMove(grid []string, pos position, direction rune) (position, rune) {
+func getMove(grid [][]string, pos position, direction rune) (position, rune) {
+	up := position{pos.r - 1, pos.c}
+	down := position{pos.r + 1, pos.c}
+	left := position{pos.r, pos.c - 1}
+	right := position{pos.r, pos.c + 1}
+
 	switch direction {
 	case '^':
 		// up
-		// check for obstacle
-		pos = position{pos.r - 1, pos.c}
-		if positionObstacle(grid, pos) {
-			// turn right
-			pos = position{pos.r + 1, pos.c + 1}
+		pos = up
+		direction = '^'
+		if positionObstacle(grid, up) {
+			pos = right
 			direction = '>'
+		} else {
+			break
+		}
+		if positionObstacle(grid, right) {
+			pos = down
+			direction = 'v'
 		}
 	case 'v':
 		// down
-		pos = position{pos.r + 1, pos.c}
-		if positionObstacle(grid, pos) {
-			// turn right
-			pos = position{pos.r - 1, pos.c - 1}
+		pos = down
+		direction = 'v'
+		if positionObstacle(grid, down) {
+			pos = left
 			direction = '<'
+		} else {
+			break
+		}
+		if positionObstacle(grid, left) {
+			pos = up
+			direction = '^'
 		}
 	case '<':
 		// left
-		pos = position{pos.r, pos.c - 1}
-		if positionObstacle(grid, pos) {
-			// turn right
-			pos = position{pos.r - 1, pos.c + 1}
+		pos = left
+		direction = '<'
+		if positionObstacle(grid, left) {
+			pos = up
 			direction = '^'
+		} else {
+			break
+		}
+		if positionObstacle(grid, up) {
+			pos = right
+			direction = '>'
 		}
 	case '>':
-		pos = position{pos.r, pos.c + 1}
-		if positionObstacle(grid, pos) {
-			// turn right
-			pos = position{pos.r + 1, pos.c - 1}
+		pos = right
+		direction = '>'
+		if positionObstacle(grid, right) {
+			pos = down
 			direction = 'v'
+		} else {
+			break
 		}
-
+		if positionObstacle(grid, down) {
+			pos = left
+			direction = '<'
+		}
 	}
 
 	return pos, direction
 }
 
+func checkForCycle(grid [][]string, loc, newObj position, direction rune) bool {
+	path := map[cyclePosition]struct{}{}
+	path[cyclePosition{loc, direction}] = struct{}{}
+
+	if grid[newObj.r][newObj.c] == "#" {
+		return false
+	}
+	grid[newObj.r][newObj.c] = "#"
+
+	defer func() {
+		grid[newObj.r][newObj.c] = "."
+	}()
+
+	for positionValid(grid, loc) {
+		loc, direction = getMove(grid, loc, direction)
+		if _, ok := path[cyclePosition{loc, direction}]; ok {
+			return true
+		}
+		path[cyclePosition{loc, direction}] = struct{}{}
+	}
+
+	return false
+}
+
 func part1() {
 	positions := map[position]struct{}{}
-	grid := []string{}
+	grid := [][]string{}
 	loc := position{-1, -1}
 	direction := '^'
 
 	lineFunc := func(line string, n int) error {
-		grid = append(grid, line)
-
+		row := []string{}
 		for i, c := range line {
+			row = append(row, string(c))
 			if c == direction {
 				loc = position{n, i}
 			}
 		}
+		grid = append(grid, row)
 
 		return nil
 	}
@@ -92,7 +156,6 @@ func part1() {
 	}
 
 	for positionValid(grid, loc) {
-		fmt.Println(loc, direction)
 		positions[loc] = struct{}{}
 		loc, direction = getMove(grid, loc, direction)
 	}
@@ -100,6 +163,49 @@ func part1() {
 	fmt.Println(len(positions))
 }
 
+func part2() {
+	grid := [][]string{}
+	loc := position{-1, -1}
+	direction := '^'
+
+	lineFunc := func(line string, n int) error {
+		row := []string{}
+		for i, c := range line {
+			row = append(row, string(c))
+			if c == direction {
+				loc = position{n, i}
+			}
+		}
+		grid = append(grid, row)
+
+		return nil
+	}
+	err := utils.ReadFile("input.txt", lineFunc)
+	if err != nil {
+		panic(err)
+	}
+
+	cycles := 0
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == "#" {
+				continue
+			}
+			if loc.r == i && loc.c == j {
+				continue
+			}
+
+			if checkForCycle(grid, loc, position{i, j}, '^') {
+				cycles++
+			}
+		}
+	}
+
+	fmt.Println(cycles)
+}
+
 func main() {
 	part1()
+	part2()
 }
